57001d24faa4860cf73edc0764d95efb179788a7
SanjayaWJinasena <sanjaya.w@jinasena.com.lk>
2026-04-07 10:47:28 +0530

    Remove references

--- models/res_partner_bank.py
+++ models/res_partner_bank.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResPartnerBank(models.Model):
@@ -7,21 +7,5 @@ class ResPartnerBank(models.Model):
     x_branch_code = fields.Char(
         string='Branch Code',
         size=3,
-        help='3-digit CBC branch code for SLIPS transfers. Auto-filled when a Branch is selected.',
+        help='3-digit CBC branch code for SLIPS transfers.',
     )
-    branch_id = fields.Many2one(
-        'jinasena.bank.branch',
-        string='Branch',
-        domain="[('bank_id', '=', bank_id)]",
-        help='Select the bank branch. Branch Code is filled automatically.',
-    )
-
-    @api.onchange('bank_id')
-    def _onchange_bank_id(self):
-        self.branch_id = False
-        self.x_branch_code = False
-
-    @api.onchange('branch_id')
-    def _onchange_branch_id(self):
-        if self.branch_id:
-            self.x_branch_code = self.branch_id.branch_code
